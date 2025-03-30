a46469cbb1aa4c2b701c028ef7b47960dde706f4
kongbai <2913156428@qq.com>
2025-03-30 14:02:48 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,26 @@
-const CACHE_NAME = 'rollcall-v4.0';
-const PRECACHE_URLS = [
-    '/',
-    '/index.html',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
-];
+const CACHE_NAME = 'random-call-final';
+const CACHE_FILES = ['/', '/index.html'];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(PRECACHE_URLS))
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(CACHE_FILES))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => key !== CACHE_NAME && caches.delete(key))
+    ).then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => 
-            Promise.all(keys.map(key => 
-                key !== CACHE_NAME ? caches.delete(key) : null
-            )
-        )
-    );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
+      .catch(() => caches.match('/'))
+  );
 });
